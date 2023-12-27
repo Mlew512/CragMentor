daa075ea8f28df052160bbd7d76c0fc64d340b48
Derek Marion <derek.j.marion@gmail.com>
2023-12-27 09:30:02 -0800

    Add basic user authentication and modeling

--- backend/user_app/models.py
+++ backend/user_app/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    email = models.EmailField(
+        verbose_name="email address",
+        max_length=255,
+        unique=True,
+    )
+
+    # Add your additional fields here
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    # Add any other required fields
+    current_level = models.IntegerField(null=True)
+    goal = models.IntegerField(null=True)
+    location = models.FloatField(null=True)
+    distance_willing_to_travel = models.IntegerField(null=True)
+
+    # Add related_name to avoid clashes with auth.User
+    groups = models.ManyToManyField(
+        "auth.Group",
+        blank=True,
+        related_name="user_groups",
+        related_query_name="user_group",
+    )
+    user_permissions = models.ManyToManyField(
+        "auth.Permission",
+        blank=True,
+        related_name="user_permissions",
+        related_query_name="user_permission",
+    )
--- backend/user_app/views.py
+++ backend/user_app/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from .models import User
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.status import (
+    HTTP_201_CREATED,
+    HTTP_204_NO_CONTENT,
+    HTTP_200_OK,
+    HTTP_404_NOT_FOUND,
+    HTTP_400_BAD_REQUEST,
+)
+from rest_framework.authtoken.models import Token
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from .serializers import UserSerializer
+
 
 # Create your views here.
+class Sign_Up(APIView):
+    def post(self, request):
+        request.data["username"] = request.data["email"]
+        user = User.objects.filter(email=request.data["email"]).first()
+        if not user:
+            user = User.objects.create_user(**request.data)
+        elif user:
+            return Response(
+                f"User {user.email} already exists", status=HTTP_400_BAD_REQUEST
+            )
+        token = Token.objects.create(user=user)
+        return Response(
+            {"user": user.email, "token": token.key}, status=HTTP_201_CREATED
+        )
+
+
+class Log_in(APIView):
+    def post(self, request):
+        email = request.data.get("email")
+        password = request.data.get("password")
+        user = authenticate(username=email, password=password)
+        if user:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response(
+                {"user": user.email, "token": token.key}, status=HTTP_200_OK
+            )
+        return Response("User not found", HTTP_404_NOT_FOUND)
+
+
+class Log_out(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        request.user.auth_token.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
+
+
+class Info(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = User.objects.get(email=request.user.email)
+        user_serialized = UserSerializer(user)
+        if user_serialized:
+            return Response(user_serialized.data, HTTP_200_OK)
+        return Response("User not found", HTTP_404_NOT_FOUND)
