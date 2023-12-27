90e440880b7ea38c4797706ed673b33513a0a213
Derek Marion <derek.j.marion@gmail.com>
2023-12-27 09:28:59 -0800

    Add necessary apps to settings

--- backend/crag_proj/settings.py
+++ backend/crag_proj/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'pyramid_app',
     'routes_app',
     'user_app',
@@ -78,8 +80,8 @@ WSGI_APPLICATION = 'crag_proj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cragmentor',
     }
 }
 
@@ -124,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Authentication
+AUTH_USER_MODEL = 'user_app.User'
