857697a775a80110a9e27fca74b41fb7eeeae794
pradobanz <pradobanz@gmail.com>
2023-12-27 08:48:44 -1000

    Add Images and title

--- frontend/src/component/Header.jsx
+++ frontend/src/component/Header.jsx
@@ -8,7 +8,7 @@ const Header = ()=>{
     <>
       <Navbar bg="light" data-bs-theme="light">
         <Container>
-          <Navbar.Brand href="#home"><img src={Logo} style={{width:"50px", height:"50px"}} /></Navbar.Brand>
+          <Navbar.Brand href="#home"><img src={Logo} style={{width:"70px", height:"70px"}} /></Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Login</Nav.Link>
