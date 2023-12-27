e5d903e53fc84a4c5e649418dffd85b3c25f2f7c
Michael Termotto <mjtermotto11@gmail.com>
2023-12-27 17:51:30 +0100

    Added login, signup, master, info, logout functions

--- backend/crag_proj/settings.py
+++ backend/crag_proj/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'pyramid_app',
     'routes_app',
     'user_app',
@@ -125,5 +127,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
 AUTH_USER_MODEL = 'user_app.User'
