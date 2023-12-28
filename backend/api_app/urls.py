4ced999648dc7ace489d43044a7f843b9ef15548
Mattlewis2014@gmail.com <mattlewis2014@gmail.com>
2023-12-28 15:23:27 -0500

    adjusted url for area and updated readme

--- backend/api_app/urls.py
+++ backend/api_app/urls.py
@@ -3,6 +3,6 @@ from .views import OpenBetaView, GetArea, GetClimbView
 
 urlpatterns = [
     path('', OpenBetaView.as_view(), name="beta"),
-    path('area', GetArea.as_view(), name="area"),
+    path('area/', GetArea.as_view(), name="area"),
     path('climb/', GetClimbView.as_view(), name="climb"),
 ]
