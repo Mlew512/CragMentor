e81bb04f2f930157b24d71a92ccab4836243fd47
Mattlewis2014@gmail.com <mattlewis2014@gmail.com>
2023-12-27 22:51:15 -0500

    added resources for api call in queries.py

--- plans/queries.py
+++ plans/queries.py
@@ -21,7 +21,7 @@ query getClimbById {
 """
 
 # gets all crags in the area defined by the lat long and max distance, we can run this and get all crags that we can pull in. possibly by mapping through uuid of crags. also aggrigates the climbs in each area by #of climbs in each grade. we could write an algorithm to order what crags we go through first. (for example we need #1 v7 #2 v6 #4 v5s (Keeping it simple) we would weight the responses of v5s to be worth 4, v6's worth 2, and v7 worth 1 when sorting the crags. all routes not in our pyramid worth 0 )
-# considerations with the algorithm, 1. grades are formatted multiple ways v7 v7- v7+ v6/v7, v7/v8. slash grades should be considered the higher grade.  
+# considerations with the algorithm, 1. grades are formatted multiple ways v7 v7- v7+ v6/v7, v7/v8. slash grades should be considered the higher grade.
 
 GET_CRAGS_IN_AREA = """
 query getCragsInAreaV2 {
@@ -39,6 +39,11 @@ query getCragsInAreaV2 {
           label
         }
       }
+    metadata {
+      lat
+      lng
+      areaId
+    }
     }
   }
 }
@@ -57,6 +62,91 @@ query getClimbsInCrag {
     }
     uuid
     totalClimbs
+  }
+}
+"""
+
+
+otherq= """
+query getCragsInArea {
+  cragsNear(
+    includeCrags: true
+    lnglat: {lat: 35.0285833, lng: -85.283861}
+    maxDistance: 100000
+  ) {
+    crags {
+      areaName
+      totalClimbs
+      aggregate {
+        byGrade {
+          count
+          label
+        }
+      }
+      metadata {
+        areaId
+        lat
+        lng
+      }
+    }
+    _id
+  }
+}
+	"""
+
+
+json_body_post_for_Pyramid = 
+"""{
+  "goal_grade": 5,
+  "location": {
+    "lat": 37.7749,
+    "lng": -122.4194
+  },
+  "maxDistance": 20000
+  
+} 
+"""
+
+pyramid_format_response = """
+{
+  "my_pyramid": {
+    "pyramid": {
+      "goal_climb": {
+        "name": "The Girls' Problem (sit)",
+        "grade": "V5",
+        "uuid": "2c460ef8-20f6-54bc-aeb8-004b77d2015e"
+      },
+      "runner_up_1": {
+        "name": "Black Shine",
+        "grade": "V4",
+        "uuid": "21ecc9c5-540c-59d8-a523-693aba07afa3"
+      },
+      "runner_up_2": {
+        "name": "Over the Bulge",
+        "grade": "V3",
+        "uuid": "1c01a859-c3bf-59de-ac7b-7e228f114ee7"
+      },
+      "runner_up_3": {
+        "name": "Over the Bulge",
+        "grade": "V3",
+        "uuid": "1c01a859-c3bf-59de-ac7b-7e228f114ee7"
+      },
+      "runner_up_4": {
+        "name": "Over the Bulge",
+        "grade": "V3",
+        "uuid": "1c01a859-c3bf-59de-ac7b-7e228f114ee7"
+      },
+      "runner_up_5": {
+        "name": "Over the Bulge",
+        "grade": "V3",
+        "uuid": "1c01a859-c3bf-59de-ac7b-7e228f114ee7"
+      },
+      "runner_up_6": {
+        "name": "Over the Bulge",
+        "grade": "V3",
+        "uuid": "1c01a859-c3bf-59de-ac7b-7e228f114ee7"
+      }
+    }
   }
 }
 """
