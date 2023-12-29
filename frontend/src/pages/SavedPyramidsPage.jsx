8070e56f6df13f4258a0fe35a573f3eac3a6fad3
Halsey Filbin <halsey.t.filbin@gmail.com>
2023-12-29 17:01:01 -0600

    Profile page updated

---
+++ frontend/src/pages/SavedPyramidsPage.jsx
@@ -0,0 +1,48 @@
+// import React, { useEffect } from "react";
+// import { Container, Row, Col, Card } from "react-bootstrap";
+// import { useNavigate, useOutletContext } from "react-router-dom";
+// import { api } from "../utilities";
+
+// const SavedPyramidsPage = () => {
+//     const { user, setSavedPyramids } = useOutletContext();
+//     const navigate = useNavigate();
+
+//     useEffect(() => {
+//         const fetchSavedPyramids = async () => {
+//             try {
+//                 const response = await api.get("/api/getSavedPyramids");
+
+//                 if (response.status === 200) {
+//                     setSavedPyramids(response.data.savedPyramids);
+//                 }
+//             } catch (error) {
+//                 console.error("Error fetching saved pyramids:", error);
+//             }
+//         };
+
+//         fetchSavedPyramids();
+//     }, [setSavedPyramids]);
+
+//     return (
+//         <>
+//             <Container>
+//                 <Row>
+//                     <Col>
+//                         <h3>Saved Pyramids</h3>
+//                         {user && user.savedPyramids && user.savedPyramids.length > 0 ? (
+//                             user.savedPyramids.map((pyramid) => (
+//                                 <Card key={pyramid.id}>
+//                                     {/* info about each saved pyramid */}
+//                                 </Card>
+//                             ))
+//                         ) : (
+//                             <p>No saved pyramids.</p>
+//                         )}
+//                     </Col>
+//                 </Row>
+//             </Container>
+//         </>
+//     );
+// };
+
+// export default SavedPyramidsPage;
