2b4e2fce37fb7d45cf565299fb3d0349b839e0db
Mattlewis2014@gmail.com <mattlewis2014@gmail.com>
2024-01-01 11:23:20 -0500

    added best crag endpoint

--- backend/api_app/urls.py
+++ backend/api_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import OpenBetaView, GetArea, GetClimbView, CragsBounds,CragsNear
+from .views import OpenBetaView, GetArea, GetClimbView, CragsBounds,CragsNear, BestCragView
 
 urlpatterns = [
     path('', OpenBetaView.as_view(), name="beta"),
@@ -7,4 +7,5 @@ urlpatterns = [
     path('climb/', GetClimbView.as_view(), name="climb"),
     path('cragsnear/', CragsNear.as_view(), name="cragsnear"),
     path('climb-bounds/', CragsBounds.as_view(), name="climb_bounds"),
+    path('best-crag/', BestCragView.as_view(), name="best_crag"),
 ]
