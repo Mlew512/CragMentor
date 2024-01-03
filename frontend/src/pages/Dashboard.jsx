d1b2eb87500cee740eab47681acb2e3f792f20ea
Michael Termotto <mjtermotto11@gmail.com>
2024-01-03 21:42:30 +0100

    Fixed duplicate fav

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -148,43 +148,7 @@ const Dashboard =()=>{
             </Card>
           </Col>
         </Row>
-        <Row>
-          <Col>
-            <Card>
-              <CardHeader className="text-center">Favorites</CardHeader>
-              <CardBody>
-
-
-                <Table striped>
-                  <thead>
-                    <tr className="text-center">
-                      <th>Name</th>
-                      <th>View</th>
-                      <th>Delete</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {Array.isArray(favoriteRoutes) && favoriteRoutes.length > 0 ? (
-                      favoriteRoutes.map((fav, index) => (
-                        <tr key={index} className="text-center">
-                          <td><Link to={`/${fav.areaName != null ? 'area' : 'route'}/${fav.uuid}`}>{fav.areaName != null ? fav.areaName : fav.name}</Link></td>
-                    
-                          <td>View</td>
-                          <td><FavButton data={fav} /></td>
-                          
-                        </tr>
-                      ))
-                    ) : (
-                      <tr>
-                        <td colSpan="6">No data available</td>
-                      </tr>
-                    )}
-                  </tbody>
-                </Table>
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
+  
       </Container>
     </>
   );
