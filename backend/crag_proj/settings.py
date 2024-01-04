3699c0b17ee938881f6bc170880dcca4e9cb0b62
Derek Marion <derek.j.marion@gmail.com>
2024-01-04 08:57:52 -0800

    Configuration changes to load dev/prod values in env

--- backend/crag_proj/settings.py
+++ backend/crag_proj/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv("../.env.development")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-awe@k=d#o#f2z@la7lt0s_yix=@ui5n^ffs9g**m%x$6qy9j30"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 
