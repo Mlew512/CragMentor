0db8c20eea2b2c44b7bd9602b7cdd8f8f2aefeb7
Mattlewis2014@gmail.com <mattlewis2014@gmail.com>
2024-01-05 17:31:32 -0500

    styling route page

--- frontend/src/pages/RoutePage.jsx
+++ frontend/src/pages/RoutePage.jsx
@@ -1,91 +1,83 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useOutletContext,useParams } from 'react-router-dom'
-import { useNavigate } from 'react-router-dom';
-import { getAPI, endpoints, postAPI } from '../utilities/api';
+import { Link, useParams } from 'react-router-dom';
+import { postAPI, endpoints } from '../utilities/api';
 import MapView from '../component/MapView';
-import DisplayMessage from '../component/DisplayMessage'
-import LoadingSpinner from '../component/LoadingSpinner'
-import FavButton from "../component/FavButton"
+import DisplayMessage from '../component/DisplayMessage';
+import LoadingSpinner from '../component/LoadingSpinner';
+import FavButton from '../component/FavButton';
+
+// Import Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './RoutePage.css'; // Create a CSS file for custom styling
 
 function RoutePage() {
-    const [dataID, setDataID] = useState(null)
-    const [data, setData] = useState(null)
-    const [message, setMessage] = useState("")
-    const [isLoading, setIsLoading] = useState(true)
-    const params = useParams();
-    const navigate = useNavigate();
+  const [dataID, setDataID] = useState(null);
+  const [data, setData] = useState(null);
+  const [message, setMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const params = useParams();
 
-    useEffect(()=>{
-        let id = null
-        if(params.id)id = params.id
-        if(dataID != id){
-            setDataID(id)
-        }
-      },[params])
+  useEffect(() => {
+    let id = null;
+    if (params.id) id = params.id;
+    if (dataID !== id) {
+      setDataID(id);
+    }
+  }, [params]);
 
-      useEffect(()=>{
-        setIsLoading(true)
-        getData()
-      },[dataID])
+  useEffect(() => {
+    setIsLoading(true);
+    getData();
+  }, [dataID]);
 
-      const temp = async () =>{
+  const temp = async () =>{
 
-      }
-    const getData = async () => {
-        const response = await postAPI(endpoints.route,null,{uuid:dataID})
-        console.log(response)
-        if(response.status && response.data.climb_data.climb != null){
-            setData(response.data.climb_data.climb)
-            setIsLoading(false)
-            setMessage('')
-        }else{
-            setMessage('Something went wrong!')
-            setData(null)
-            setIsLoading(false)
-        }
+  }
 
+  const getData = async () => {
+    const response = await postAPI(endpoints.route, null, { uuid: dataID });
+    console.log(response);
+    if (response.status && response.data.climb_data.climb != null) {
+      setData(response.data.climb_data.climb);
+      setIsLoading(false);
+      setMessage('');
+    } else {
+      setMessage('Something went wrong!');
+      setData(null);
+      setIsLoading(false);
+    }
+  };
 
-
-    };
-   
-
-    return (
+  return (
+    <>
+      <DisplayMessage message={message} />
+      {isLoading === false && data != null ? (
         <>
-        
-        
+          <div className="route-details-container">
+            <div className="left-column">
+              {data.media.length > 0 && (
+                <img className="route-image" src={`https://media.openbeta.io/${data.media[0]['mediaUrl']}`} alt={data.name} />
+              )}
+              <h2>{data.name}</h2>
+              <p>Grade: {data.grades?.vscale}</p>
+              <FavButton data={data} />
+            </div>
+            <div className="right-column">
+              <p>Description - {data.content.description}</p>
+              <p>Location - {data.content.location}</p>
+              <Link to={`/area/${data.parent.uuid}`}>
+                <button className="btn btn-primary">Go to {data.parent.area_name}</button>
+              </Link>
+            </div>
+          </div>
 
-        <DisplayMessage message={message} />
-        {
-            isLoading == false && data != null ?
-            (
-                <>
-                <section>  
-                    {
-                        data.media.length > 0 &&
-                        <img style={{width:'100px'}} src={"https://media.openbeta.io/" + data.media[0]['mediaUrl']}/>
-                    }
-                    {data.name}
-                    <FavButton data={data} />
-                    {data.boltsCount != -1 && <p>boltsCount - {data.boltsCount}</p>}
-                    {data.length != -1 && <p>length - {data.length}</p>}
-                    {data.gradeContext != null && <p>gradeContext - {data.gradeContext}</p>}         
-                    <p>description - {data.content.description}</p>
-                    <p>location - {data.content.location}</p>
-                    <p>protection - {data.content.protection}</p>
-                    <p>safety - {data.safety}</p>
-                    <a href={"/area/"+data.parent.uuid}>Go to {data.parent.area_name}</a>
-
-                </section>
-
-                <MapView data={[data]} centerOnFirst={true} showSearch={false} boundsChangedCallback={temp} />
-                </>
-            ):
-            (<LoadingSpinner isLoading={isLoading} />)
-        }
+          <MapView data={[data]} centerOnFirst={true} showSearch={false} boundsChangedCallback={temp} />
         </>
-    )
-
-
+      ) : (
+        <LoadingSpinner isLoading={isLoading} />
+      )}
+    </>
+  );
 }
 
 export default RoutePage;
