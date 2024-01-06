7b5ae9ec36af90ca9c16fcd8a8a1560632751209
Mattlewis2014@gmail.com <mattlewis2014@gmail.com>
2024-01-06 10:28:23 -0500

    style 404 page

--- frontend/src/pages/Error404Page.jsx
+++ frontend/src/pages/Error404Page.jsx
@@ -1,14 +1,21 @@
-import { Link } from "react-router-dom";
-const Error404Page = () => {
-    return (
-        <>
-            <div>This page does not exist</div>
-            <div>
-                <Link to='/'>Back</Link>
-            </div>
-        </>
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-    )
+const Error404Page = () => {
+    const navigate = useNavigate();
+
+    return (
+        <div className="d-flex flex-column align-items-center justify-content-center vh-100">
+            <div className="text-center mb-4">
+                <h1 className="display-4">This page does not exist.</h1>
+            </div>
+            <div>
+                <Link className="btn btn-primary" onClick={() => navigate(-1)}>
+                    Go Back
+                </Link>
+            </div>
+        </div>
+    );
 }
 
 export default Error404Page;
