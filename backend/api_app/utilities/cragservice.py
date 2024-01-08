1fd389c576b79d87adc2e3a0ffb38e994d7f50eb
Michael Termotto <mjtermotto11@gmail.com>
2024-01-08 16:27:40 +0100

    Increase timeout for query

--- backend/api_app/utilities/cragservice.py
+++ backend/api_app/utilities/cragservice.py
@@ -15,7 +15,7 @@ class CragService:
             response = requests.post(
                 "https://api.openbeta.io/",
                 json={"query": crag_in_area_query, "variables": variables},
-                timeout=10,
+                timeout=20,
             )
 
             response.raise_for_status()
@@ -40,7 +40,7 @@ class CragService:
             response = requests.post(
                 "https://api.openbeta.io/",
                 json={"query": climbs_in_crag_query, "variables": variables},
-                timeout=5,
+                timeout=20,
             )
 
             response.raise_for_status()
@@ -131,7 +131,7 @@ class CragService:
             response = requests.post(
                 "https://api.openbeta.io/",
                 json={"query": query, "variables": variables},
-                timeout=10,
+                timeout=20,
             )
 
             response.raise_for_status()
@@ -155,7 +155,7 @@ class CragService:
             response = requests.post(
                 "https://api.openbeta.io/",
                 json={"query": get_climb_by_id_query, "variables": variables},
-                timeout=10,
+                timeout=20,
             )
 
             response.raise_for_status()
