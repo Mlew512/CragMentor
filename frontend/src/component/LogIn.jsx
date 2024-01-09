dc63f34436adfe450d50078dfe20d502838fe7f0
pradobanz <pradobanz@gmail.com>
2024-01-09 04:56:42 -1000

    Fixed navigate hook Pyramid Page

--- frontend/src/component/LogIn.jsx
+++ frontend/src/component/LogIn.jsx
@@ -30,7 +30,7 @@ const LogIn = ({setExistingUser, existingUser}) => {
       setAuth(response.data.token)
       // setAuth(response.data.token)
       // api.defaults.headers.common["Authorization"] = `Token ${response.data.token}`;
-      navigate("/dashboard/");
+      navigate("/pyramid/");
       window.location.reload();
     } catch (error) {
       setError("Invalid email or password. Please try again.");
